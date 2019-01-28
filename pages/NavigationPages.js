968f4ef5e0b4e22b48d1b821bcb3d395bffff4ac
Robert Rees <robert@wegotpop.com>
2019-01-28 15:12:07 +0000

    Removes the CSS module for the navigation pages

--- pages/NavigationPages.js
+++ pages/NavigationPages.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import styles from './css/NavigationPage.css';
-
 export function NavigationPage() {
   return (
     <main>
@@ -11,7 +9,7 @@ export function NavigationPage() {
         website. They are useful when you have a large number of potential
         onwards journies that no longer work effectively in navigation bars.
       </p>
-      <ul className={styles['navigation-page-links']}>
+      <ul className="navigation-page-links">
         <li>
           <a href="#">Page one</a>
         </li>
