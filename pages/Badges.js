b04d20d9c4a2acbd871fceb67348de9b451b48e2
Amit Shah <amit@amwam.me>
2019-03-29 17:52:46 +0000

    Add Badges to the styleguide

---
+++ pages/Badges.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import badgeStyles from '../css/badge.css';
+import className from 'classname';
+import pageStyles from './css/BadgesPage.css';
+
+function badge(name, style) {
+  return (
+    <div className={pageStyles.item}>
+      <span
+        className={className(badgeStyles.badge, badgeStyles[`badge-${style}`])}
+      >
+        {name}
+      </span>
+      <div>
+        class: <code>.badge {style && `.badge-${style}`}</code>
+      </div>
+    </div>
+  );
+}
+
+export function Badges() {
+  return (
+    <div>
+      <h2>Badges</h2>
+      {badge('Default')}
+      {badge('Positive', 'positive')}
+      {badge('Info', 'info')}
+      {badge('Warning', 'warning')}
+      {badge('Error', 'error')}
+    </div>
+  );
+}
--- pages/Navigation.js
+++ pages/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { MenuItem } from './components/MenuItem';
+import styles from './css/NavigationPage.css';
 
 export default props => (
   <div>
     <h2>Navigation</h2>
     <h3>In-page navigation</h3>
-    <ul>
+    <ul className={styles['spaced-list']}>
       <MenuItem onClick={props.onClick} href="/fast-links" title="Fast links" />
     </ul>
   </div>
