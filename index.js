b73d56532f76ac282118458966f423e7d5e19986
John Haynes <john.haynes@wegotpop.com>
2019-07-18 12:32:04 +0100

    Updated sidebar and added principles
    I have updated the home page with new text, changed the layout of the sidebar and added a new page to include the UX Principles

--- index.js
+++ index.js
@@ -27,13 +27,41 @@ import { Microcopy } from './pages/Microcopy';
 import { Badges } from './pages/Badges';
 import { WidgetPages } from './pages/WidgetPages';
 import { PageText } from './pages/PageText';
+import { UXPrinciples } from './pages/UXPrinciples';
 import { DeviceBreakpoints } from './pages/DeviceBreakpoints';
 
 const PAGES = {
   home: () => (
     <div>
-      <h1>We Got POP - Style guide</h1>
-      <em>Work in progress</em>
+      <section>
+        <h1>We Got POP - Style guide</h1>
+        <p>
+          Predictable, consistent design enables users to focus on the task at
+          hand, rather than how to use our service. This Styleguide covers the
+          look and feel of the POP environment its components.
+        </p>
+        <hr />
+      </section>
+      <section>
+        <h2>Environment</h2>
+        <p>
+          The environment for all POP’s services is based on the guidelines of
+          Google’s Material Design; the depth, attributes and behaviour of all
+          components are designed to emulate real-world behaviours.
+        </p>
+        <p>
+          Surfaces; objects cannot occupy the same space simultaneously, nor
+          pass through each other. Objects can change shape and move along any
+          axis. Their motion can be independent, or synchronised.
+        </p>
+        <p>
+          Depth; elevation is used to provide a visual hierarchy. Items in the
+          foreground take precedence over those in the background and scrims can
+          be used to provide focus. Shadows are used to express elevation and
+          must be used consistently. Shadow size reflects elevation; higher
+          elevation casts larger shadows.
+        </p>
+      </section>
     </div>
   ),
   '/colours': Colours,
@@ -49,6 +77,7 @@ const PAGES = {
   '/case-headers': CaseHeaders,
   '/page-headings': PageHeadings,
   '/page-text': PageText,
+  '/ux-principles': UXPrinciples,
   '/notifications': Notifications,
   '/content-containers': ContentContainers,
   '/forms': FormElements,
--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -5,53 +5,70 @@ import { MenuItem } from './MenuItem';
 
 export const Sidebar = ({ onClick }) => (
   <div id={styles['sidebar']}>
-    <h2 id={styles['sidebar-title']}>POP PRODUCT DESIGN</h2>
+    <h2 id={styles['sidebar-title']}>POP STYLE GUIDE</h2>
     <ul className={styles['sidebar-menu']}>
       <MenuItem onClick={onClick} href="/" title="Home" />
     </ul>
 
     <ul className={styles['sidebar-menu']}>
       <h4>Principles</h4>
-      <MenuItem onClick={onClick} href="/colours" title="Colour palette" />
+      <MenuItem onClick={onClick} href="/ux-principles" title="UX Principles" />
+    </ul>
+
+    <ul className={styles['sidebar-menu']}>
+      <h4>Guidelines</h4>
+      <br />
+      <h5>Colours</h5>
+      <MenuItem onClick={onClick} href="/colours" title="Colour Palette" />
+      <MenuItem title="Using Colours" />
+      <h5>Text</h5>
       <MenuItem onClick={onClick} href="/typography" title="Typography" />
       <MenuItem onClick={onClick} href="/language" title="Language and text" />
       <MenuItem onClick={onClick} href="/microcopy" title="Microcopy" />
       <MenuItem onClick={onClick} href="/navigation" title="Navigation" />
-      <MenuItem onClick={onClick} href="/animation" title="Animation" />
-      <MenuItem onClick={onClick} href="/widget-pages" title="Widget pages" />
+      {/*<MenuItem onClick={onClick} href="/animation" title="Animation" />*/}
     </ul>
 
     <ul className={styles['sidebar-menu']}>
       <h4>Components</h4>
+      <br />
+      <h5>Navigation</h5>
+      <MenuItem onClick={onClick} href="/navigation" title="Navigation Bar" />
       <MenuItem
         onClick={onClick}
         href="/navigation-pages"
-        title="Navigation pages"
+        title="Navigation Pages"
       />
-      <MenuItem
-        onClick={onClick}
-        href="/icons_and_media"
-        title="Icons and media"
-      />
-      <MenuItem onClick={onClick} href="/page-headings" title="Page headings" />
-      <MenuItem onClick={onClick} href="/page-text" title="Page text" />
-      <MenuItem onClick={onClick} href="/forms" title="Form elements" />
-      <MenuItem onClick={onClick} href="/buttons" title="Buttons and links" />
-      <MenuItem onClick={onClick} href="/badges" title="Badges" />
-      <MenuItem onClick={onClick} href="/fast-links" title="Fast links" />
-      <MenuItem onClick={onClick} href="/tables" title="Tables and lists" />
-      <MenuItem onClick={onClick} href="/case-headers" title="Case headers" />
-      <MenuItem
-        onClick={onClick}
-        href="/content-containers"
-        title="Content containers (Panels)"
-      />
-      <MenuItem onClick={onClick} href="/notifications" title="Notifications" />
+      <MenuItem title="Breadcrumbs" />
+      <MenuItem title="Pagination" />
+      <h5>Layout</h5>
       <MenuItem
         onClick={onClick}
         href="/device-breakpoints"
         title="Device Breakpoints"
       />
+      <MenuItem onClick={onClick} href="/widget-pages" title="Widget pages" />
+      <h5>Headers</h5>
+      <MenuItem onClick={onClick} href="/page-headings" title="Page Headings" />
+      <MenuItem onClick={onClick} href="/case-headers" title="Case Headers" />
+      <MenuItem onClick={onClick} href="/page-text" title="Page Text" />
+      <MenuItem
+        onClick={onClick}
+        href="/fast-links"
+        title="Fast Links and Tabs"
+      />
+      <h5>Elements</h5>
+      <MenuItem onClick={onClick} href="/content-containers" title="Panels" />
+      <MenuItem onClick={onClick} href="/buttons" title="Buttons and Links" />
+      <MenuItem onClick={onClick} href="/tables" title="Tables and lists" />
+      <MenuItem onClick={onClick} href="/forms" title="Forms" />
+      <MenuItem
+        onClick={onClick}
+        href="/icons_and_media"
+        title="Icons and media"
+      />
+      <MenuItem onClick={onClick} href="/badges" title="Badges" />
+      <MenuItem onClick={onClick} href="/notifications" title="Notifications" />
     </ul>
     <ul className={styles['sidebar-menu']}>
       <h4>Other</h4>
