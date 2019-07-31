781233dc94b8f2c4230c2f3d423517142e9b866f
John Haynes <john.haynes@wegotpop.com>
2019-07-31 20:52:42 +0100

    Added documentation for buttons
    - Split`buttons` and `links` into individual pages; added new content for button positioning, as per notes from the recent meeting
    - Added a new page for button colours
    - Modified the sidebar to account for the new page structure

---
+++ pages/ButtonColours.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import tableStyles from '../css/tables.css';
+import linkStyles from '../css/link.css';
+import ButtonDiagram from './assets/button-diagram.svg';
+import buttonStyles from '../css/button.css';
+import styles from './css/ButtonPage.css';
+
+function ButtonExample({ label, colour, cssClass, blurb }) {
+  return (
+    <div className={styles['button-section']}>
+      <h4>{label}</h4>
+      <div>{blurb}</div>
+      <br />
+      <div>
+        CSS class: &nbsp;<code>{cssClass}</code>
+      </div>
+      <div>
+        <button className={`${buttonStyles.btn} ${buttonStyles[cssClass]}`}>
+          Action
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default () => {
+  return (
+    <div>
+      <section>
+        <h3>Colours</h3>
+        <br />
+        <ButtonExample
+          label="Default/Secondary Action"
+          colour="White"
+          cssClass="btn-default"
+          blurb="This is the default button colour; unless a button requires "
+        />
+
+        <ButtonExample
+          label="Primary Action"
+          colour="Teal"
+          cssClass="btn-primary"
+          blurb="Teal colour is the defaut colour across platform, associated with primary actions."
+        />
+
+        <ButtonExample
+          label="Info"
+          colour="Blue"
+          cssClass="btn-info"
+          blurb="Blue colour is associated with secondary actions that don't afect your workflow and that are considered as safe"
+        />
+
+        <ButtonExample
+          label="Success"
+          colour="Green"
+          cssClass="btn-success"
+          blurb="Green colour is associated with positive actions on the platform."
+        />
+
+        <ButtonExample
+          label="Warning"
+          colour="Yellow"
+          cssClass="btn-warning"
+          blurb=""
+        />
+
+        <ButtonExample
+          label="Danger"
+          colour="Red"
+          cssClass="btn-danger"
+          blurb="Red colour is associated with negative or destructive actions that have an immediate affect on the workflow."
+        />
+      </section>
+    </div>
+  );
+};
--- pages/ButtonsAndLinks.js
+++ pages/ButtonsAndLinks.js
@@ -4,6 +4,7 @@ import linkStyles from '../css/link.css';
 import ButtonDiagram from './assets/button-diagram.svg';
 import buttonStyles from '../css/button.css';
 import styles from './css/ButtonPage.css';
+import { MenuItem } from './components/MenuItem';
 
 function ButtonExample({ label, colour, cssClass, blurb }) {
   return (
@@ -23,19 +24,23 @@ function ButtonExample({ label, colour, cssClass, blurb }) {
   );
 }
 
-export default () => {
+export default props => {
   return (
     <div>
       <section>
         <h1>Buttons</h1>
         <p>
-          Buttons are used to fire actions. For example, If the screen view
-          changes to an entirely different document, a link control should be
-          used. If the form on the screen is simply altered, a button control
-          should be used.
+          Buttons are used to fire actions; the nature of the button's action
+          may change the button style, content and positioning. This page has
+          been constructed to assist you in understanding how to implement the
+          buttons your require.
         </p>
       </section>
       <section>
+        <h2>
+          <strong>Button Content</strong>
+        </h2>
+        <br />
         <h3>Labelling</h3>
         <p>
           Labelling for buttons should be descriptive and will usually follow{' '}
@@ -49,15 +54,38 @@ export default () => {
         </p>
       </section>
       <section>
-        <h3>Button Types</h3>
+        <h3>Primary and Secondary Actions</h3>
+        <br />
+        <ButtonExample
+          label="Default/Secondary Action"
+          colour="White"
+          cssClass="btn-default"
+          blurb="This is the default button colour; unless the action is deemed as primary (or when a colour is deemed appropriate, such as destructive actions."
+        />
+
+        <ButtonExample
+          label="Primary Action"
+          colour="Teal"
+          cssClass="btn-primary"
+          blurb="Teal colour is the defaut colour across platform, associated with primary actions."
+        />
+        <p>For more information about using colours with buttons, read the</p>
+        <MenuItem
+          onClick={props.onClick}
+          href="/button-colours"
+          title="Button Colour Guide"
+        />
+      </section>
+      <section>
+        <h2>
+          <strong>Button Types</strong>
+        </h2>
         <br />
         <h4>Singular Actions</h4>
         <p>
           Button labels should always use title case, as this helps to
           distinguish the button from other nearby text.
         </p>
-        <h5>Example</h5>
-        <br />
         <button className={`${buttonStyles.btn}`}>Action</button>
         <br />
         <br />
@@ -68,112 +96,14 @@ export default () => {
           this button type should only be used when the nested options are a
           subtype of the action that’s in the drop down button.
         </p>
-        <h5>Labelling</h5>
         <p>
           The label of a dropdown button should follow the{' '}
           <code>(action)(object)</code> syntax, as usual.
         </p>
-        <h5>Example</h5>
         <p>
-          If multiple versions of the same object can be created, then a
-          dropdown is used. For example, multiple engagement types can be
-          selected from a `New Engagement` button.{' '}
+          <strong>[Example TBC, Dropdown Button]</strong>
         </p>
         <br />
-        <p>
-          <strong>Example TBC</strong>
-        </p>
-      </section>
-      {/*<section>
-        <h3>Buttons</h3>
-        <ButtonDiagram />
-      </section>*/}
-      <section>
-        <h3>Colours</h3>
-        <br />
-        <ButtonExample
-          label="Default/Secondary Action"
-          colour="White"
-          cssClass="btn-default"
-          blurb="This is the default button colour; unless a button requires "
-        />
-
-        <ButtonExample
-          label="Primary Action"
-          colour="Teal"
-          cssClass="btn-primary"
-          blurb="Teal colour is the defaut colour across platform, associated with primary actions."
-        />
-
-        <ButtonExample
-          label="Info"
-          colour="Blue"
-          cssClass="btn-info"
-          blurb="Blue colour is associated with secondary actions that don't afect your workflow and that are considered as safe"
-        />
-
-        <ButtonExample
-          label="Success"
-          colour="Green"
-          cssClass="btn-success"
-          blurb="Green colour is associated with positive actions on the platform."
-        />
-
-        <ButtonExample
-          label="Warning"
-          colour="Yellow"
-          cssClass="btn-warning"
-          blurb=""
-        />
-
-        <ButtonExample
-          label="Danger"
-          colour="Red"
-          cssClass="btn-danger"
-          blurb="Red colour is associated with negative or destructive actions that have an immediate affect on the workflow."
-        />
-
-        {/*<ButtonExample
-          label="Reject"
-          colour="Black"
-          cssClass="btn-reject"
-          blurb="Black colour is associated with returning actions or actions that might affect your workflow, although not instantly. "
-        />
-
-        <ButtonExample
-          label="Alteration"
-          colour="Grey"
-          cssClass="btn-alteration"
-          blurb="Grey colour is associated with a change on the system or on a content."
-        />*/}
-      </section>
-      <section>
-        <h3>Button Placement</h3>
-        <h4>Tables and Lists</h4>
-        <p>
-          Due to the volume of actions associated to tables and lists, it's
-          possible for these components to necessitate a high number of buttons;
-          because of this, buttons have been grouped into a series of button
-          bars:
-        </p>
-        <ul>
-          <li>
-            Actions; actions that modify or transform data within a table/list
-            should be placed above the table/list, aligned to the left. The
-            primary action should be the left-aligned button. Secondary actions
-            should be grouped to the right of the primary action.
-          </li>
-          <li>
-            Tools; tooling that enables users to search, select, sort or filter
-            a table/list should be placed above the table/list, aligned to the
-            left
-          </li>
-          <li></li>
-        </ul>
-        <p></p>
-        <p></p>
-      </section>
-      <section>
         <h3>States</h3>
         <br />
         <table className={tableStyles.table}>
@@ -210,6 +140,70 @@ export default () => {
             </tr>
           </tbody>
         </table>
+      </section>
+      <section>
+        <h2>
+          <strong>Positioning</strong>
+        </h2>
+        <br />
+        <h3>Ordering</h3>
+        <br />
+        <p>
+          Buttons should align to the top and left. The primary action should be
+          the left-aligned button. Secondary actions should be grouped to the
+          right of the primary action.
+        </p>
+        <h3>Placement</h3>
+        <br />
+        <h4>Tables and Lists</h4>
+        <p>
+          Due to the volume of actions associated to tables and lists, it's
+          possible for these components to necessitate a high number of buttons;
+          because of this, buttons have been grouped into a series of button
+          bars:
+        </p>
+        <ul>
+          <li>Actions; actions that create, modify or transform data</li>
+          <li>
+            Tools; buttons that enable users to search, select, sort or filter
+          </li>
+          <li>
+            Pagination; buttons that enable the user to control the results they
+            are viewing
+          </li>
+        </ul>
+        <p>
+          On table and list pages, the button bars are duplicated above and
+          below the table/list content (appearing in reverse order when
+          beneath).
+        </p>
+        <ol>
+          <li>Actions</li>
+          <li>Tools</li>
+          <li>Pagination</li>
+          <li>Table / List</li>
+          <li>Pagination</li>
+          <li>Tools</li>
+          <li>Actions</li>
+        </ol>
+        <br />
+        <h4>Forms & Widget Pages</h4>
+        <p>
+          Actions on widget pages should be grouped into a button bar and to be
+          placed in a container at the top and left of the content they relate
+          to.
+        </p>
+        <h5>Single Panel Forms</h5>
+        <p>
+          Actions that enable users to edit the contents of a panel should
+          appear within the panel, below the panel header and left-aligned.
+        </p>
+        <h5>Multiple Panel Forms</h5>
+        <p>
+          When a form is split across multiple panels, actions that save or
+          submit a form should be placed outside of the last panel, aligned to
+          the left (to align with the "F pattern").
+        </p>
       </section>
     </div>
   );
