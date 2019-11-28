7870ed8332b217135bfaab4c8e98bd3320a8ebc6
Patrik <57677593+patrikmpop@users.noreply.github.com>
2019-11-28 15:38:07 +0000

    Update ContentContainers.js

--- pages/ContentContainers.js
+++ pages/ContentContainers.js
@@ -139,27 +139,3 @@ export default () => {
     </div>
   );
 };
-
-// export default () => (
-//   <div>
-//     <h2>Content Container (Panels)</h2>
-//     <p>
-//       Primary containers often hold dashboard widgets, charts, or other
-//       information on a page. They're most often used to segment information on a
-//       dashboard, review harness, or landing page.
-//     </p>
-//     <br />
-//     <div className={panelStyles.panel}>
-//       <div className={panelStyles['panel-heading']}>
-//         <h3>Heading goes here</h3>
-//       </div>
-//       <div className={panelStyles['panel-body']}>
-//         Content
-//         <br />
-//         More content
-//         <br />
-//         Even more content
-//       </div>
-//     </div>
-//   </div>
-// );
