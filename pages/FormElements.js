9b7e498edc9a7f1972a1fc3749b3a161f6735a20
Amit Shah <amit@wegotpop.com>
2020-06-08 08:47:42 +0100

    Update CSS to include to form alert styles (#39)

--- pages/FormElements.js
+++ pages/FormElements.js
@@ -14,11 +14,52 @@ export default () => (
       Here, we’ve defined the common input types, styles, modes, and layouts
       that POP uses to drive results.
     </p>
-    <div>
-      <img
-        className={styles.image}
-        src={require('./assets/form-diagram.png')}
-      />
-    </div>
+
+    <section>
+      <h3>Alerts</h3>
+      <p>
+        Alerts contained inside a <code>form-group</code> will have slightly
+        different styling.
+      </p>
+      <p>
+        These are used to highlight errors/warnings and other bits of
+        information about an input
+      </p>
+      <div className="form-group">
+        <div className="alert alert-info">
+          This is an <code>alert alert-info</code>
+        </div>
+        <div className="alert alert-success">
+          This is an <code>alert alert-success</code>
+        </div>
+        <div className="alert alert-warning">
+          This is an <code>alert alert-warning</code>
+        </div>
+        <div className="alert alert-danger">
+          This is an <code>alert alert-danger</code>
+        </div>
+      </div>
+    </section>
+    <section>
+      <h3>Required Inputs</h3>
+      <p>Required fields are prepended with a red asterisk (*).</p>
+      <p>
+        <code>control-label field-required</code>.
+      </p>
+      <label className="control-label field-required" htmlFor="text">
+        Required Text Input
+      </label>
+      <input className="form-control" type="text" name="text" />
+    </section>
+
+    <section>
+      <h3>Flow Diagram</h3>
+      <div>
+        <img
+          className={styles.image}
+          src={require('./assets/form-diagram.png')}
+        />
+      </div>
+    </section>
   </div>
 );
